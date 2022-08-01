e343776504b83101d6a398e709b8206cb2e92431
rickytuan <d.fed16@gptgdt.net>
2022-08-02 00:24:15 +0300

    first commit

---
+++ app/app.js
@@ -0,0 +1,19 @@
+import { serve } from "https://deno.land/std@0.120.0/http/server.ts";
+
+const handleRequest = (request) => {
+  const url = new URL(request.url);
+  const params = url.searchParams;
+
+  const one = Number(params.get("one"));
+  const two = Number(params.get("two"));
+
+  return new Response(`Sum: ${one + two}`);
+};
+
+
+if (Deno.args.length > 0) {
+  const lastArgument = Deno.args[Deno.args.length - 1];
+  port = Number(lastArgument);
+}
+
+serve(handleRequest, { port: 7777 });
